3a2816ed8fa9eed6d32901c78dd3f2d856dc1668
n4djib <n4djib@gmail.com>
2024-05-21 18:34:03 +0100

    edited navbar

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -29,7 +29,7 @@ export const Route = createRootRoute({
               offset: 0,
             }}
           >
-            Pokemon
+            Pokemons
           </Link>
           <Link to="/buttons" activeProps={activeProps}>
             Buttons
