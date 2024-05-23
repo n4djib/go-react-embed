63d2fc714da78c164b5b08b966287b8757a8ea52
n4djib <n4djib@gmail.com>
2024-05-23 21:47:06 +0100

    created signup and signin api

--- api/users.go
+++ api/users.go
@@ -3,20 +3,131 @@ package api
 import (
 	"go-react-embed/models"
 	"net/http"
+	"os"
 	"strconv"
+	"time"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/labstack/echo/v4"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func RegisterUsersHandlers(e *echo.Group) {
 	e.GET("/users", getUsersHandler)
 	e.GET("/users/:id", getUserHandler)
 	e.GET("/users/name/:name", getUserByNameHandler)
-	e.POST("/users", createUserHandler)
+	// e.POST("/users", createUserHandler)
 	e.PUT("/users", updateUserHandler)
 	e.DELETE("/users/:id", deleteUserHandler)
 	e.PUT("/users/active-state", updateUserActiveStateHandler)
+
+	
+	e.POST("/auth/signup", signup)
+	e.POST("/auth/signin", signin)
 }
+
+func signup(c echo.Context) error {
+	var body models.CreateUserParams
+	if err := c.Bind(&body); err != nil {
+		return c.JSON(http.StatusBadRequest, Error{
+			Error: err.Error(),
+		})
+	}
+	// Validate the data
+	if err := validate.Struct(body); err != nil {
+		return c.JSON(http.StatusBadRequest, Error{
+			Error: err.Error(),
+		})
+	}
+	// check user name doesn't exist
+	foundUser, err := models.QUERIES.GetUserByName(models.CTX, body.Name)
+	if err != nil && err.Error() != "sql: no rows in result set" {
+		return c.JSON(http.StatusInternalServerError, Error{
+			Error: err.Error(),
+		})
+	}
+	if foundUser.ID != 0 {
+		return c.JSON(http.StatusInternalServerError, Error{
+			Error: "Duplicate user name",
+		})
+	}
+
+	// check password strength
+
+	// hash password
+	hash, err := bcrypt.GenerateFromPassword([]byte(body.Password), 10)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Error{
+			Error: "Failed to hash password",
+		})
+	}
+	body.Password = string(hash)
+
+	// insert it
+	user, err := models.QUERIES.CreateUser(models.CTX, body)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Error{
+			Error: err.Error(),
+		})
+	}
+	return c.JSON(http.StatusOK, Status{
+		Message: "Signed up successfully",
+		Data:    user,
+	})
+}
+
+
+func signin(c echo.Context) error {
+	var body models.CreateUserParams
+	if err := c.Bind(&body); err != nil {
+		return c.JSON(http.StatusBadRequest, Error{
+			Error: err.Error(),
+		})
+	}
+	// Validate the data
+	if err := validate.Struct(body); err != nil {
+		return c.JSON(http.StatusBadRequest, Error{
+			Error: err.Error(),
+		})
+	}
+
+	// check user name exist
+	user, err := models.QUERIES.GetUserByNameWithPassword(models.CTX, body.Name)
+	if err != nil || user.ID == 0 {
+		return c.JSON(http.StatusInternalServerError, Error{
+			Error: "user name or password are incorrect 1",
+		})
+	}
+
+	// check password
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Error{
+			Error: "user name or password are incorrect 2",
+		})
+	}
+
+	// generate JWT token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": user.ID,
+		"exp": time.Now().Add(time.Hour * 24 * 30).Unix(),
+	})
+	// sign the token and encode a a string
+	signedToken, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Error{
+			Error: "Failed to create token\n "+ err.Error(),
+		})
+	}
+
+	user.Password = "[HIDDEN]"
+	return c.JSON(http.StatusOK, echo.Map{
+		"Message": "Sign in successfully",
+		"Data": user,  // we should return JWT
+		"token": signedToken,
+	})
+}
+
 
 func getUsersHandler(c echo.Context) error {
 	users, err := models.QUERIES.ListUsers(models.CTX)
@@ -67,30 +178,30 @@ func getUserByNameHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, data)
 }
 
-func createUserHandler(c echo.Context) error {
-	var body models.CreateUserParams
-	if err := c.Bind(&body); err != nil {
-		return c.JSON(http.StatusBadRequest, Error{
-			Error: err.Error(),
-		})
-	}
-	// Validate the data
-	if err := validate.Struct(body); err != nil {
-		return c.JSON(http.StatusBadRequest, Error{
-			Error: err.Error(),
-		})
-	}
-	user, err := models.QUERIES.CreateUser(models.CTX, body)
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, Error{
-			Error: err.Error(),
-		})
-	}
-	return c.JSON(http.StatusOK, Status{
-		Message: "created successfully",
-		Data:    user,
-	})
-}
+// func createUserHandler(c echo.Context) error {
+// 	var body models.CreateUserParams
+// 	if err := c.Bind(&body); err != nil {
+// 		return c.JSON(http.StatusBadRequest, Error{
+// 			Error: err.Error(),
+// 		})
+// 	}
+// 	// Validate the data
+// 	if err := validate.Struct(body); err != nil {
+// 		return c.JSON(http.StatusBadRequest, Error{
+// 			Error: err.Error(),
+// 		})
+// 	}
+// 	user, err := models.QUERIES.CreateUser(models.CTX, body)
+// 	if err != nil {
+// 		return c.JSON(http.StatusInternalServerError, Error{
+// 			Error: err.Error(),
+// 		})
+// 	}
+// 	return c.JSON(http.StatusOK, Status{
+// 		Message: "created successfully",
+// 		Data:    user,
+// 	})
+// }
 
 func updateUserHandler(c echo.Context) error {
 	var body models.UpdateUserParams
--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -21,9 +22,11 @@ require (
 	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/crypto v0.22.0 // indirect
-	golang.org/x/net v0.24.0 // indirect
-	golang.org/x/sys v0.19.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/crypto v0.23.0 // indirect
+	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
+	golang.org/x/tools v0.21.0 // indirect
 )
